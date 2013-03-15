75fa59d8f3974c3e715d826360433a5474989a88
Tessa Thornton <tessathornton@gmail.com>
2013-03-15 15:44:41 -0400

    add cname

--- Gruntfile.js
+++ Gruntfile.js
@@ -228,6 +228,7 @@ module.exports = function (grunt) {
                dest: '<%= yeoman.dist %>',
                src: [
                  '*.{ico,txt}',
+                 'CNAME',
                  '.htaccess'
                ]
              }, {
